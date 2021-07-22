37bf044e259a80efb4c40b7585c7d3d616d1448d
Bananza93 <zubryaev.sergey@gmail.com>
2021-07-22 12:14:50 +0400

    A&DS/Lesson 2 Homework

---
+++ src/main/java/ru/geekbrains/algo_and_data_struct/lesson2/Lesson2_HW.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.algo_and_data_struct.lesson2;
+
+public class Lesson2_HW {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/ru/geekbrains/algo_and_data_struct/lesson2/Vendor.java
@@ -0,0 +1,4 @@
+package ru.geekbrains.algo_and_data_struct.lesson2;
+
+public class Vendor {
+}
