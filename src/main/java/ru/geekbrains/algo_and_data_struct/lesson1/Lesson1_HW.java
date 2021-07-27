d32fdfc94e5bd487d53c52f28e82e81fe1e755a4
Bananza93 <zubryaev.sergey@gmail.com>
2021-07-27 16:57:15 +0400

    A&DS/Lesson 2 Homework

--- src/main/java/ru/geekbrains/algo_and_data_struct/lesson1/Lesson1_HW.java
+++ src/main/java/ru/geekbrains/algo_and_data_struct/lesson1/Lesson1_HW.java
@@ -2,7 +2,7 @@ package ru.geekbrains.algo_and_data_struct.lesson1;
 
 import java.math.BigInteger;
 
-public class Test {
+public class Lesson1_HW {
 
     public static void main(String[] args) {
         ex1();
