bde784ae2f3ca671d1e78bbaf2467af463332d49
Bananza93 <zubryaev.sergey@gmail.com>
2021-07-27 16:53:36 +0400

    A&DS/Lesson 2 Homework

--- src/main/java/ru/geekbrains/algo_and_data_struct/lesson1/Test.java
+++ src/main/java/ru/geekbrains/algo_and_data_struct/lesson1/Test.java
@@ -1,127 +1,63 @@
 package ru.geekbrains.algo_and_data_struct.lesson1;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 
 public class Test {
-    private static long iter = 0;
-    private static long factIter = 0;
-    private static long fibIter = 0;
 
     public static void main(String[] args) {
-        long start = System.nanoTime();
-//        System.out.println(getSimpleDividers1(600851475143L));
-//        System.out.println("Time = " + ((double) (System.nanoTime() - start) / 1000000000));
-//        start = System.nanoTime();
-//        System.out.println(Arrays.deepToString(fillArrayBySpiralMethod(2,3)));
-/*        int[][] arr = fillArrayBySpiralMethod(3, 1);
-        for (int[] arr1 : arr) {
-            for (int i : arr1) {
-                System.out.print(i + "  ");
-            }
-            System.out.println();
-        }*/
+        ex1();
+        ex2();
+        ex3();
+    }
 
+    private static void ex1() {
+        System.out.println("Ex. 1");
+        long number = 600851475143L;
+        long start = System.currentTimeMillis();
+        long divider = getMaxPrimeDivider(number);
+        long end = System.currentTimeMillis();
+        System.out.println("For " + number + " max prime divider is " + divider + ". Time = " + (end - start) + " ms.");
+        System.out.println();
+    }
+
+    private static void ex2() {
+        System.out.println("Ex. 2");
+        print2DArray(fillArrayBySpiralMethod(3, 1));
+        System.out.println();
+        print2DArray(fillArrayBySpiralMethod(2, 3));
+        System.out.println();
+        print2DArray(fillArrayBySpiralMethod(4, 4));
+        System.out.println();
+    }
+
+    private static void ex3() {
+        System.out.println("Ex. 3");
         String a = "11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111";
-//        String b = "123456789123456789123456789123451234567891234567891234567891234512345678912345678912345678912345";
-//        String a = "123";
-        String b = "3211";
-        String res = addBigNumber(a,b);
-        BigInteger resB = new BigInteger(a).add(new BigInteger(b));
-        System.out.println(res + "\n" + resB);
-        System.out.println(res.equals(resB.toString()));
-//        System.out.println(getSimpleDividers2(1092));
-//        System.out.println("Time = " + ((double) (System.nanoTime() - start) / 1000000000));
-//        testA();
-//        System.out.println("After test A: " + iter);
-//        iter = 0;
-//        testB();
-//        System.out.println("After test B: " + iter);
-//        iter = 0;
-//        testC();
-//        System.out.println("After test C: " + iter);
-//        long start = System.currentTimeMillis();
-//        factorial(BigInteger.valueOf(5000));
-//        System.out.println("Iter = " + factIter + " | Time = " + ((double)(System.currentTimeMillis() - start) / 1000));
-//        start = System.currentTimeMillis();
-//        fib(BigInteger.valueOf(41));
-//        System.out.println("Iter = " + fibIter + " | Time = " + ((double)(System.currentTimeMillis() - start) / 1000));
+        String b = "123456789123456789123456789123451234567891234567891234567891234512345678912345678912345678912345";
+        String c = "123";
+        String d = "3211";
+        addAndCheckBigNumbers(a, b);
+        addAndCheckBigNumbers(a, c);
+        addAndCheckBigNumbers(c, d);
+        addAndCheckBigNumbers(d, b);
     }
 
-    private static void testC() {
-        int n = 10000;
-        List<Integer> arrayList = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                iter++;
-                arrayList.add(i * j);
-                n--;
-            }
+    private static void addAndCheckBigNumbers(String a, String b) {
+        String res = addBigNumber(a, b);
+        BigInteger resBI = new BigInteger(a).add(new BigInteger(b));
+        System.out.println(res + "\n" + resBI);
+        System.out.println(res.equals(resBI.toString()));
+        System.out.println();
+    }
+
+    private static void print2DArray(int[][] arr) {
+        for (int[] arr1 : arr) {
+            for (int i : arr1) System.out.print(i + "  ");
+            System.out.println();
         }
     }
 
-    private static void testB() {
-        int n = 10000;
-        List<Integer> arrayList = new ArrayList<>();
-        for (int i = 0; i < n; i += 2) {
-            for (int j = i; j < n; j++) {
-                iter++;
-                arrayList.add(i * j);
-            }
-        }
-    }
-
-    public static void testA() {
-        int n = 10000;
-        List<Integer> arrayList = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j < n; j *= 2) {
-                iter++;
-                arrayList.add(i * j);
-            }
-        }
-    }
-
-    public static BigInteger factorial(BigInteger n) {
-        factIter++;
-        if (n.equals(BigInteger.ONE)) {
-            return n;
-        }
-        return n.multiply(factorial(n.subtract(BigInteger.valueOf(1))));
-    }
-
-    public static BigInteger fib(BigInteger n) {
-        fibIter++;
-        if (n.equals(BigInteger.ONE)) {
-            return BigInteger.ZERO;
-        }
-        if (n.equals(BigInteger.TWO)) {
-            return BigInteger.ONE;
-        }
-        return fib(n.subtract(BigInteger.ONE)).add(fib(n.subtract(BigInteger.TWO)));
-    }
-
-    public static Long getSimpleDividers1(long number) {
-        long maxThreshold = number / 2;
-        for (int i = 2; i < maxThreshold; i++) {
-            if ((double) number % i == 0) {
-                long currDivider = number / i;
-                for (int j = 2; j < currDivider / 2; j++) {
-                    if (currDivider % j == 0) {
-                        currDivider = -1;
-                        break;
-                    }
-                }
-                if (currDivider != -1) return currDivider;
-            }
-        }
-        return -1L;
-    }
-
-    private static Long getSimpleDividers2(long number) {
+    private static Long getMaxPrimeDivider(long number) {
         long d = 2;
         while (d * d <= number) {
             if (number % d == 0) number /= d;
