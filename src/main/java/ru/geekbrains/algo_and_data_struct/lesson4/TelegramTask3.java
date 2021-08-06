778a0a345c07908a74e9272db345b1215ceb46f0
Bananza93 <zubryaev.sergey@gmail.com>
2021-08-07 00:23:27 +0400

    A&DS/Lesson 4 Homework

--- src/main/java/ru/geekbrains/algo_and_data_struct/lesson4/TelegramTask3.java
+++ src/main/java/ru/geekbrains/algo_and_data_struct/lesson4/TelegramTask3.java
@@ -10,12 +10,12 @@ public class TelegramTask3 {
         return currNumber;
     }
 
-    private static boolean isPrime(int n) {
-        if (n <= 1) return false;
-        if (n <= 3) return true;
-        if (n % 2 == 0 || n % 3 == 0) return false;
-        for (int i = 5; i <= Math.sqrt(n); i += 6){
-            if (n % i == 0 || n % (i + 2) == 0) return false;
+    private static boolean isPrime(int number) {
+        if (number <= 1) return false;
+        if (number <= 3) return true;
+        if (number % 2 == 0 || number % 3 == 0) return false;
+        for (int i = 5; i <= Math.sqrt(number); i += 6){
+            if (number % i == 0 || number % (i + 2) == 0) return false;
         }
         return true;
     }
