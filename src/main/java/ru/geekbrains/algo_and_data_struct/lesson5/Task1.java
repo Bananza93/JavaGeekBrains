4c0ed6df09172e7d51c858b6afe4f4637026a0ca
Bananza93 <zubryaev.sergey@gmail.com>
2021-08-09 23:40:25 +0400

    A&DS/Lesson 5 Homework

---
+++ src/main/java/ru/geekbrains/algo_and_data_struct/lesson5/Task1.java
@@ -0,0 +1,18 @@
+package ru.geekbrains.algo_and_data_struct.lesson5;
+
+public class Task1 {
+    public static double calculatePower(int base, int exponent) {
+        if (base == 0) {
+            if (exponent > 0) return 0;
+            if (exponent < 0) throw new IllegalArgumentException("For base = 0 exponent must be >= 0");
+        }
+        if (exponent == 0 || base == 1) return 1;
+        if (base == -1) return exponent % 2 == 0 ? 1 : -1;
+        return calculatePower1(exponent > 0 ? base : 1 / (double) base, Math.abs(exponent));
+    }
+
+    private static double calculatePower1(double base, int exponent) {
+        if (exponent == 0) return 1;
+        return base * calculatePower1(base, exponent - 1);
+    }
+}
