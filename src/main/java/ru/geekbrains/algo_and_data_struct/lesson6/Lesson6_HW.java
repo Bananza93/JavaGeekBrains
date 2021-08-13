5ba836f57d2c2cd16e9cde2ccf282cea9150f302
Bananza93 <zubryaev.sergey@gmail.com>
2021-08-13 22:39:07 +0400

    A&DS/Lesson 6 Homework

--- src/main/java/ru/geekbrains/algo_and_data_struct/lesson6/Lesson6_HW.java
+++ src/main/java/ru/geekbrains/algo_and_data_struct/lesson6/Lesson6_HW.java
@@ -3,7 +3,7 @@ package ru.geekbrains.algo_and_data_struct.lesson6;
 public class Lesson6_HW {
     public static void main(String[] args) {
         int maxLevel = 6;
-        int treeNumber = 3;
+        int treeNumber = 20;
         int balancedTreeCount = 0;
         for (int i = 0; i < treeNumber; i++) {
             BinaryTree<Integer> tree = new BinaryTreeImpl<>(maxLevel);
